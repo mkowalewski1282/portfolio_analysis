1bd19e27538c5dae611435dfdfebd85226cef618
mkowalewski1282 <mfk1282@gmail.com>
2024-10-21 14:43:41 +0200

    gaussian copula fir added

--- copula_fit.R
+++ copula_fit.R
@@ -10,40 +10,53 @@ library(MASS)
 library(ks)
 
 return_rates = read.csv("data/positive_quarterly_return_rates.csv")
-
 stocks = colnames(return_rates)[2:ncol(return_rates)]
 
 ecdf_list = lapply(return_rates[stocks], ecdf)
 
-
 ## example plot
 plot(ecdf(return_rates$AAPL), main="ECDF of AAPL", xlab="Return", ylab="ECDF")
 
-
 pseudo_obs = apply(return_rates[stocks], 2, function(x) rank(x)/(length(x)+1))
-
-
 dim = ncol(pseudo_obs) 
-clayton_cop = claytonCopula(dim=dim)
-fit = fitCopula(clayton_cop, pseudo_obs, method="ml")   # maximum likelihood estimation (ml)
 
+
+
+###### CLAYTON
+clayton_cop = claytonCopula(dim=dim)
+clayton_fit = fitCopula(clayton_cop, pseudo_obs, method="ml")   # maximum likelihood estimation (ml)
 
 # summary(fit)
-
-theta = coef(fit)
+theta = coef(clayton_fit)
 # print(theta)
 
-
-simulated_data <- rCopula(1000, claytonCopula(theta, dim))
+simulated_data_clayton = rCopula(1000, claytonCopula(theta, dim))
+colnames(simulated_data_clayton) = stocks
 # head(simulated_data)
 
 # first two stocks -> countour plot
-plot(simulated_data[,1], simulated_data[,2], main="Simulated Clayton Copula Data",
+plot(simulated_data_clayton[,1], simulated_data_clayton[,2], main="Simulated Clayton Copula Data",
      xlab="Stock 1", ylab="Stock 2")
 
+write.csv(simulated_data_clayton, file = "data/copulas_outputs/simulated_clayton_all_stocks.csv", row.names = FALSE)
 
 
+###### GAUSSIAN COPULA
+
+gaussian_cop = normalCopula(dim = dim, dispstr = "un")  # Use "un" for an unstructured copula
+fit_gaussian = fitCopula(gaussian_cop, pseudo_obs, method = "ml")  # Maximum Likelihood Estimation
+
+rho = coef(fit_gaussian)
+
+simulated_data_gaussian <- rCopula(1000, normalCopula(rho, dim, dispstr = "un"))
+colnames(simulated_data_gaussian) = stocks
+
+# Plot Gaussian copula simulation for the first two stocks
+plot(simulated_data_gaussian[,1], simulated_data_gaussian[,2], 
+     main = "Simulated Gaussian Copula Data",
+     xlab = "Stock 1", ylab = "Stock 2")
 
 
+write.csv(simulated_data_gaussian, file = "data/copulas_outputs/simulated_gaussian_all_stocks.csv", row.names = FALSE)
 
 
