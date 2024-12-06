c79f6bde90cdb541304cfc9d7fc4c759b8a32977
mkowalewski1282 <mfk1282@gmail.com>
2024-12-06 01:36:40 +0100

    plots for THESIS

--- copula_fit_test.R
+++ copula_fit_test.R
@@ -31,11 +31,11 @@ plot_density <- function(simulated_data) {
   y <- simulated_data[, 2]
   density_estimate <- kde2d(x, y, n = 50)
   par(mfrow = c(1, 2))  # layout
-  image(density_estimate, main = "2D Density Plot", xlab = "Stock 1", ylab = "Stock 2")
+  image(density_estimate, main = "Wykres konturowy", xlab = "u1", ylab = "u2")
   contour(density_estimate, add = TRUE) # image with contour overlay
   persp(density_estimate$x, density_estimate$y, density_estimate$z,
         theta = 30, phi = 20, expand = 0.5, col = "lightblue",
-        xlab = "Stock 1", ylab = "Stock 2", zlab = "Density", main = "3D Density Plot",
+        xlab = "u1", ylab = "u2", zlab = "", main = "Wykres gętości",
         ticktype = "detailed", axes = TRUE)
   par(mfrow = c(1, 1))
 }
@@ -49,6 +49,9 @@ clayton_fit = fitCopula(clayton_cop, pseudo_obs, method="ml")   # maximum likeli
 
 # summary(fit)
 theta = coef(clayton_fit)
+theta = 0.2 # ONLY FOR THESIS
+theta = 0.5 # ONLY FOR THESIS
+theta = 5   # ONLY FOR THESIS
 # print(theta)
 simulated_data_clayton = rCopula(100000, claytonCopula(theta, dim))
 colnames(simulated_data_clayton) = stocks
@@ -57,7 +60,7 @@ colnames(simulated_data_clayton) = stocks
 plot(simulated_data_clayton[,1], simulated_data_clayton[,2], main="Simulated Clayton Copula Data",
      xlab="Stock 1", ylab="Stock 2")
 plot_density(simulated_data_clayton)
-write.csv(simulated_data_clayton, file = "data/copulas_outputs/simulated_clayton_random_22_stocks.csv", row.names = FALSE)
+#write.csv(simulated_data_clayton, file = "data/copulas_outputs/simulated_clayton_random_22_stocks.csv", row.names = FALSE)
 
 
 ###### GAUSSIAN COPULA
@@ -65,6 +68,10 @@ write.csv(simulated_data_clayton, file = "data/copulas_outputs/simulated_clayton
 gaussian_cop = normalCopula(dim = dim, dispstr = "un")  # Use "un" for an unstructured copula
 fit_gaussian = fitCopula(gaussian_cop, pseudo_obs, method = "ml")  # Maximum Likelihood Estimation
 rho = coef(fit_gaussian)
+dim = 2    # ONLY FOR THESIS
+rho = 0.2  # ONLY FOR THESIS
+rho = 0.5  # ONLY FOR THESIS
+rho = 0.95 # ONLY FOR THESIS
 simulated_data_gaussian <- rCopula(100000, normalCopula(rho, dim, dispstr = "un"))
 colnames(simulated_data_gaussian) = stocks
 # Plot Gaussian copula simulation for the first two stocks
@@ -72,7 +79,7 @@ plot(simulated_data_gaussian[,1], simulated_data_gaussian[,2],
      main = "Simulated Gaussian Copula Data",
      xlab = "Stock 1", ylab = "Stock 2")
 plot_density(simulated_data_gaussian)
-write.csv(simulated_data_gaussian, file = "data/copulas_outputs/simulated_gaussian_22_stocks.csv", row.names = FALSE)
+#write.csv(simulated_data_gaussian, file = "data/copulas_outputs/simulated_gaussian_22_stocks.csv", row.names = FALSE)
 
 
 ###### t - STUDENT COPULA
@@ -90,6 +97,13 @@ fit_t <- fitCopula(t_cop, pseudo_obs, method = "ml")
 params_t <- coef(fit_t)
 rho_t <- params_t[1:(length(params_t)-1)]  
 df_t <- params_t[length(params_t)]          
+ 
+dim = 2      # ONLY FOR THESIS
+rho_t = 0.5  # ONLY FOR THESIS
+rho_t = 0.8  # ONLY FOR THESIS
+df_t = 1     # ONLY FOR THESIS
+df_t = 2     # ONLY FOR THESIS
+df_t = 8     # ONLY FOR THESIS
 
 simulated_data_t <- rCopula(100000, tCopula(rho_t, dim, df = df_t, dispstr = "un"))
 colnames(simulated_data_t) <- stocks
@@ -98,6 +112,6 @@ plot(simulated_data_t[,1], simulated_data_t[,2],
      xlab = "Stock 1", ylab = "Stock 2")
 plot_density(simulated_data_t)
 
-write.csv(simulated_data_t, file = "data/copulas_outputs/simulated_t_student_22_stocks.csv", row.names = FALSE)
+# write.csv(simulated_data_t, file = "data/copulas_outputs/simulated_t_student_22_stocks.csv", row.names = FALSE)
 
 
